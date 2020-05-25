4304cbd646e0f111818c10c25bae33bee60d3c83
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2020-05-25 14:49:43 -0700

    Update MainActivity.java

--- Roll_Dice (with Graphics)/MainActivity.java
+++ Roll_Dice (with Graphics)/MainActivity.java
@@ -2,30 +2,38 @@ package com.example.constraintlayoutapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, Runnable {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Runnable{
     //References to Dice Image
     ImageView imgDice;
 
+    //Contains the ids of all dice drawables
     int diceIDs[];
 
+    //Used for indexing the dice images with the mod()
+    int current = -1;
+
+    //Contains the starting time
+    long start;
+
+    //Contains the ending time
+    long end;
+
+    //How long you want the cycling of images to go for (in milliseconds)
+    long cycleDuration = 3000;
 
     //References to Roll Button
     Button rollButton;
 
     //References to Display Roll #
-    TextView displayRoll;
-
-
+    TextView displayRoll, rollDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +51,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rollButton.setOnClickListener(this);
 
         displayRoll = (TextView) findViewById(R.id.displayRoll);
+        rollDisplay = (TextView) findViewById(R.id.rollDisplay);
 
     }
 
     //Starts Cycling through images for a duration, then stops displaying the right side of the dice
     public void rollDice(){
-        //Random
-        Random rand = new Random();
-
+        start = System.currentTimeMillis();
+        end = start + cycleDuration;
         run();
-
-        rollButton.setClickable(true);
     }
 
     @Override
@@ -61,13 +67,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId()){
             case R.id.rollButton:
                 //Start Time
+                rollButton.setVisibility(View.GONE);
+                rollDisplay.setVisibility(View.VISIBLE);
                 rollDice();
-                rollButton.setClickable(false);
                 break;
         }
     }
 
-    //Returns an index that we want
+    //Used for cycling through dice images
     public static int mod(int a, int b){
         int r = a % b;
         //Is r < 0? then return r + b else return r (valid)
@@ -77,8 +84,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void run() {
         //Changes the image based on current value which is altered and managed by the mod() above
-        imgDice.setImageResource((diceIDs[mod(++current, diceIDs.length)]));
-        imgDice.getHandler().postDelayed(this, 500L);
-        Log.d("Cycle Time: ", "This is being cycled afaklsdjf;aksdjf;aksjdf;aksdjfljfk;ds");
+        if(System.currentTimeMillis() < end){
+            imgDice.setImageResource((diceIDs[mod(++current, diceIDs.length)]));
+            imgDice.getHandler().postDelayed(this, 150L);
+        }else{
+            //Random Object
+            Random rand = new Random();
+
+            //Generate Random Roll
+            int generateRoll = rand.nextInt(6) + 1;
+
+            //Display correct dice image and the dice roll value
+            imgDice.setImageResource(diceIDs[generateRoll - 1]);
+            displayRoll.setText("Dice Rolled: " + generateRoll);
+
+            //Reset Button Display
+            rollButton.setVisibility(View.VISIBLE);
+            rollDisplay.setVisibility(View.GONE);
+
+            return;
+        }
     }
 }
